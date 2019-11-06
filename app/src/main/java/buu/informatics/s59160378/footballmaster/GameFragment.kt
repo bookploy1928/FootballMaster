ed87547c6c8c02698559797c10b96ef700edc1e8
book <bookploy1928@hotmail.com>
2019-11-06 15:53:30 +0700

    Navigation Home to Game

--- app/src/main/java/buu/informatics/s59160378/footballmaster/GameFragment.kt
+++ app/src/main/java/buu/informatics/s59160378/footballmaster/GameFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import buu.informatics.s59160378.footballmaster.databinding.FragmentGameBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -16,8 +18,9 @@ class GameFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        val binding = DataBindingUtil.inflate<FragmentGameBinding>(inflater,
+            R.layout.fragment_game,container,false)
+        return binding.root
     }
 
 
--- app/src/main/java/buu/informatics/s59160378/footballmaster/HomeFragment.kt
+++ app/src/main/java/buu/informatics/s59160378/footballmaster/HomeFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import buu.informatics.s59160378.footballmaster.databinding.FragmentHomeBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -15,9 +18,15 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater,
+            R.layout.fragment_home,container,false)
+
+        binding.playButton.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_homeFragment_to_gameFragment)
+        }
+        return binding.root
     }
 
 
