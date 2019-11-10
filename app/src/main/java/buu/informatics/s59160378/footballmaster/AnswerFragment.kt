2e124b7d87dd4caba6292c7020d06b297ea81088
book <bookploy1928@hotmail.com>
2019-11-10 08:10:23 +0700

    Create UI Game and Answer

--- app/src/main/java/buu/informatics/s59160378/footballmaster/AnswerFragment.kt
+++ app/src/main/java/buu/informatics/s59160378/footballmaster/AnswerFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import buu.informatics.s59160378.footballmaster.databinding.FragmentAnswerBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -16,8 +18,9 @@ class AnswerFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_answer, container, false)
+        val binding = DataBindingUtil.inflate<FragmentAnswerBinding>(inflater,
+            R.layout.fragment_answer,container,false)
+        return binding.root
     }
 
 
--- app/src/main/java/buu/informatics/s59160378/footballmaster/GameFragment.kt
+++ app/src/main/java/buu/informatics/s59160378/footballmaster/GameFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import buu.informatics.s59160378.footballmaster.databinding.FragmentGameBinding
 
 /**
@@ -20,6 +21,18 @@ class GameFragment : Fragment() {
     ): View? {
         val binding = DataBindingUtil.inflate<FragmentGameBinding>(inflater,
             R.layout.fragment_game,container,false)
+        binding.buttonAns1.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_gameFragment_to_answerFragment)
+        }
+        binding.buttonAns2.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_gameFragment_to_answerFragment)
+        }
+        binding.buttonAns3.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_gameFragment_to_answerFragment)
+        }
+        binding.buttonAns4.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_gameFragment_to_answerFragment)
+        }
         return binding.root
     }
 
